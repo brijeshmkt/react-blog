c61edfc7c5c50efcb5fcba7699e8538feecda7ab
Brijesh Mishra <brijeshmishra@Brijeshs-MacBook-Air.local>
2018-08-23 07:11:21 +0530

    routes setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import routes from './routes';
+
+const app = (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={App} />
+        </Switch>
+    </BrowserRouter>
+)
+ReactDOM.render(
+    app
+    
+    , document.getElementById('root'));
 registerServiceWorker();
